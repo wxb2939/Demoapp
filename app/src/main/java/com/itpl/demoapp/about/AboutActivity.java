042f6b4e7a7bb2853c6be5816f3cb195bd4e41c2
wxb2939 <wxb2939@github.com>
2016-09-20 10:54:52 +0800

    Common base class and method

--- app/src/main/java/com/itpl/demoapp/about/AboutActivity.java
+++ app/src/main/java/com/itpl/demoapp/about/AboutActivity.java
@@ -13,20 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.itpl.demoapp.module;
+package com.itpl.demoapp.about;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import com.itpl.demoapp.module.BaseActivity;
 
 /**
  * Created by xuebing on 16/9/20.
+ *
  */
-public class BaseActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+public class AboutActivity extends BaseActivity {
 
 }
